0e8e48a96345ce411a9fed13e395ba4668c7b7d0
cem <cemsari@outlook.com.tr>
2021-06-19 21:09:42 +0300

    Configs

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const mongoose      = require("mongoose"),
-      express       = require("express"),
-      bodyParser    = require("body-parser"),
-      app           = express();
+const mongoose       = require("mongoose"),
+      express        = require("express"),
+      passport       = require("passport"),
+      LocalStrategy  = require("passport-local"),
+      expressSession = require("express-session");
+      User           = require("./models/userModel"),
+      bodyParser     = require("body-parser"),
+      app            = express();
 
+const { request } = require("express");
 //Routes
 const indexRoutes = require("./routes/indexRoutes");
+const userModel = require("./models/userModel");
       adminRoutes = require("./routes/adminRoutes");  
 
 
 //App Config
+mongoose.connect("mongodb://localhost/Blog")
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Passport Config
+app.use(require("express-session")({
+    secret:"Guvenlik Cumlemiz",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy());
 
 
 //Routes Using
