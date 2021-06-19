15888b596be0bf66b36bf72a0741e37b710af399
cem <cemsari@outlook.com.tr>
2021-06-19 20:07:11 +0300

    endpoint name changed

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,19 +1,19 @@
 const express        = require('express'),
       router         = express.Router();
 
-router.get("/signin", (req, res)=>{
+router.get("/sign-in", (req, res)=>{
     res.render('admin/signin')
 });  
 
-router.post("/signin", (req, res)=>{
+router.post("/sign-in", (req, res)=>{
 
 });
 
-router.get("/signup", (req,res)=>{
-    res.render('admin/signup')
+router.get("/sign-up", (req,res)=>{
+    res.render('admin/sign-up')
 });
 
-router.post("/signup", (req, res)=>{
+router.post("/sign-up", (req, res)=>{
 
 });
 
